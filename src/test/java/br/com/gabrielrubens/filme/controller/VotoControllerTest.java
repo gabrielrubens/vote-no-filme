272793d9d3af068e0e5d0c0f2e77731e9dda92e6
Gabriel Rubens <gabrielrubens@gmail.com>
2014-02-01 16:42:20 -0200

    #2 [test|refa] refatoracao nos testes da disputa

--- src/test/java/br/com/gabrielrubens/filme/controller/VotoControllerTest.java
+++ src/test/java/br/com/gabrielrubens/filme/controller/VotoControllerTest.java
@@ -2,15 +2,12 @@ package br.com.gabrielrubens.filme.controller;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.util.Arrays;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 
 import br.com.caelum.vraptor.util.test.MockResult;
 import br.com.gabrielrubens.filme.model.Candidatos;
-import br.com.gabrielrubens.filme.model.Combinacao;
 import br.com.gabrielrubens.filme.model.Disputa;
 
 public class VotoControllerTest {
@@ -22,7 +19,7 @@ public class VotoControllerTest {
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 		result = new MockResult();
-		disputa = new Disputa(new Combinacao(Arrays.asList(1L, 2L, 3L, 4L, 5L), 2));
+		disputa = new Disputa();
 		controller = new VotoController(result, disputa);
 	}
 
--- src/test/java/br/com/gabrielrubens/filme/model/DisputaTest.java
+++ src/test/java/br/com/gabrielrubens/filme/model/DisputaTest.java
@@ -1,8 +1,8 @@
 package br.com.gabrielrubens.filme.model;
 
-import static org.junit.Assert.*;
-
-import java.util.Arrays;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -10,7 +10,7 @@ public class DisputaTest {
 
 	@Test
 	public void deveTer10DisputasDos5Filmes() {
-		Disputa disputa = new Disputa(new Combinacao(Arrays.asList(1L, 2L, 3L, 4L, 5L), 2));
+		Disputa disputa = new Disputa();
 		Candidatos candidatos = null;
 		
 		for(int i = 0; i < 10; i++) {
