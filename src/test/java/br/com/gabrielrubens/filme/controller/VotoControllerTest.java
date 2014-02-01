bcb6d20f0e59de8ca55e6713cfb571971879d577
Gabriel Rubens <gabrielrubens@gmail.com>
2014-02-01 16:52:28 -0200

    #2 [test|refa] refatoracao nos testes da disputa

--- src/test/java/br/com/gabrielrubens/filme/controller/VotoControllerTest.java
+++ src/test/java/br/com/gabrielrubens/filme/controller/VotoControllerTest.java
@@ -2,24 +2,31 @@ package br.com.gabrielrubens.filme.controller;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import br.com.caelum.vraptor.util.test.MockResult;
 import br.com.gabrielrubens.filme.model.Candidatos;
 import br.com.gabrielrubens.filme.model.Disputa;
+import br.com.gabrielrubens.filme.repository.FilmeRepository;
 
 public class VotoControllerTest {
 	private MockResult result;
 	private VotoController controller;
 	private Disputa disputa;
+	@Mock private FilmeRepository filmeRepository;
 	
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 		result = new MockResult();
-		disputa = new Disputa();
+		Mockito.when(filmeRepository.findIds()).thenReturn(Arrays.asList(1L,2L,3L,4L,5L));
+		disputa = new Disputa(filmeRepository);
 		controller = new VotoController(result, disputa);
 	}
 
--- src/test/java/br/com/gabrielrubens/filme/model/DisputaTest.java
+++ src/test/java/br/com/gabrielrubens/filme/model/DisputaTest.java
@@ -4,13 +4,21 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import br.com.gabrielrubens.filme.repository.FilmeRepository;
 
 public class DisputaTest {
 
 	@Test
 	public void deveTer10DisputasDos5Filmes() {
-		Disputa disputa = new Disputa();
+		FilmeRepository filmeRepository = Mockito.mock(FilmeRepository.class);
+		Mockito.when(filmeRepository.findIds()).thenReturn(Arrays.asList(1L,2L,3L,4L,5L));
+
+		Disputa disputa = new Disputa(filmeRepository);
 		Candidatos candidatos = null;
 		
 		for(int i = 0; i < 10; i++) {
