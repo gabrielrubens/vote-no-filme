5bf33ac1e08fbb07fb55694703591e359881d3ea
Gabriel Rubens <gabrielrubens@gmail.com>
2014-02-01 16:42:48 -0200

    #2 [impl|refa] refatoracao na disputa

--- src/main/java/br/com/gabrielrubens/filme/model/Disputa.java
+++ src/main/java/br/com/gabrielrubens/filme/model/Disputa.java
@@ -1,13 +1,16 @@
 package br.com.gabrielrubens.filme.model;
 
-import java.util.Iterator;
+import java.util.Arrays;
+
 
 public class Disputa {
-	private Iterator<Long[]> combinacao;
+	private final Combinacao combinacao;
 
-	public Disputa(Iterator<Long[]> combinacao) {
-		this.combinacao = combinacao;
+	
+	public Disputa() {
+		this.combinacao = new Combinacao(Arrays.asList(1L, 2L, 3L, 4L, 5L), 2);
 	}
+	
 
 	public boolean temCandidatos() {
 		return combinacao.hasNext();
