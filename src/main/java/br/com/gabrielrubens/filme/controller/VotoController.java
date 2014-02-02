5c2ed5f9fffd8c4d8466d864c9a9df87b36d92c5
Gabriel Rubens <gabrielrubens@gmail.com>
2014-02-02 02:06:57 -0200

    #4 [test] teste que simula o voto

--- src/main/java/br/com/gabrielrubens/filme/controller/VotoController.java
+++ src/main/java/br/com/gabrielrubens/filme/controller/VotoController.java
@@ -5,28 +5,47 @@ import javax.inject.Inject;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
+import br.com.gabrielrubens.filme.model.Candidatos;
 import br.com.gabrielrubens.filme.model.Disputa;
+import br.com.gabrielrubens.filme.model.Filme;
+import br.com.gabrielrubens.filme.model.Voto;
+import br.com.gabrielrubens.filme.repository.VotoRepository;
 
 @Controller
 public class VotoController {
 
 	private final Result result;
 	private final Disputa disputa;
+	private final VotoRepository repository;
 
 	@Inject
-	public VotoController(Result result, Disputa disputa) {
+	public VotoController(Result result, Disputa disputa, VotoRepository repository) {
 		this.result = result;
 		this.disputa = disputa;
+		this.repository = repository;
 	}
 	
 	public VotoController() {
-		this(null, null);
+		this(null, null, null);
 	}
 
 	@Path(value={ "/", "vote-no-filme"})
 	public void index() {
+		incluirProximoCandidato();
+	}
+
+	public void votar(Candidatos candidatos, Filme filmeVotado) {
+		repository.votar(new Voto(candidatos, filmeVotado));
+		if(incluirProximoCandidato()){
+			result.redirectTo(this).index();
+		}
+	}
+	
+	private boolean incluirProximoCandidato() {
 		if(disputa.temCandidatos()){
 			result.include("candidatos", disputa.proximosCandidatos());
+			return true;
 		}
+		return false;
 	}
 }
--- src/test/java/br/com/gabrielrubens/filme/controller/VotoControllerTest.java
+++ src/test/java/br/com/gabrielrubens/filme/controller/VotoControllerTest.java
@@ -13,13 +13,17 @@ import org.mockito.MockitoAnnotations;
 import br.com.caelum.vraptor.util.test.MockResult;
 import br.com.gabrielrubens.filme.model.Candidatos;
 import br.com.gabrielrubens.filme.model.Disputa;
+import br.com.gabrielrubens.filme.model.Filme;
+import br.com.gabrielrubens.filme.model.Voto;
 import br.com.gabrielrubens.filme.repository.FilmeRepository;
+import br.com.gabrielrubens.filme.repository.VotoRepository;
 
 public class VotoControllerTest {
 	private MockResult result;
 	private VotoController controller;
 	private Disputa disputa;
 	@Mock private FilmeRepository filmeRepository;
+	@Mock private VotoRepository votoRepository;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -27,12 +31,28 @@ public class VotoControllerTest {
 		result = new MockResult();
 		Mockito.when(filmeRepository.findIds()).thenReturn(Arrays.asList(1L,2L,3L,4L,5L));
 		disputa = new Disputa(filmeRepository);
-		controller = new VotoController(result, disputa);
+		controller = new VotoController(result, disputa, votoRepository);
 	}
 
 	@Test
 	public void deveApresentarOsCondidatos() {
 		controller.index();
+		verificaSeHaCandidatosNoResult();
+	}
+
+	@Test
+	public void deveVotarEmUmFilme(){
+		Filme filme1 = new Filme(1L, "Filme 1");
+		Filme filme2 = new Filme(2L, "Filme 2");
+		Candidatos candidatos = new Candidatos(filme1, filme2);
+		
+		controller.votar(candidatos, filme1);
+		
+		Mockito.verify(votoRepository).votar(Mockito.any(Voto.class));
+		verificaSeHaCandidatosNoResult();
+	}
+	
+	private void verificaSeHaCandidatosNoResult() {
 		Candidatos candidatos = result.included("candidatos");
 		assertNotNull(candidatos);
 		assertNotNull(candidatos.getFilme1());
