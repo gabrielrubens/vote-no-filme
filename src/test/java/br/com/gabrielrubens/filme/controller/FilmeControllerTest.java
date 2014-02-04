fab01d96ce71c9dd27ee98609ae6d29797abe488
Gabriel Rubens <gabrielrubens@gmail.com>
2014-02-03 23:48:54 -0200

    #3 [test|refa] correcao da classe

--- src/test/java/br/com/gabrielrubens/filme/controller/FilmeControllerTest.java
+++ src/test/java/br/com/gabrielrubens/filme/controller/FilmeControllerTest.java
@@ -11,14 +11,17 @@ import br.com.caelum.vraptor.util.test.MockResult;
 import br.com.gabrielrubens.filme.model.Filme;
 import br.com.gabrielrubens.filme.repository.FilmeRepository;
 import br.com.gabrielrubens.filme.repository.FilmeRepositoryImpl;
+import br.com.gabrielrubens.filme.repository.VotoRepository;
+import br.com.gabrielrubens.filme.repository.VotoRepositoryImpl;
 
 public class FilmeControllerTest {
 
 	@Test
 	public void deveCriar5Filmes() {
+		VotoRepository votoRepository = Mockito.mock(VotoRepositoryImpl.class);
 		FilmeRepository filmeRepository = Mockito.mock(FilmeRepositoryImpl.class);
 		MockResult result = new MockResult();
-		FilmeController controller = new FilmeController(result, filmeRepository);
+		FilmeController controller = new FilmeController(result, votoRepository, filmeRepository);
 		controller.inserirBaseDeTeste();
 		
 		verify(filmeRepository).removeAll();
